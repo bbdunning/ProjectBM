1c654eb562a8eaecea9a264fd61bb4a5659564ca
bbdunning <40685582+bbdunning@users.noreply.github.com>
2020-04-24 18:58:54 -0700

    finished reading in animations

--- src/main.cpp
+++ src/main.cpp
@@ -364,27 +364,47 @@ public:
 
 	Animation createAnimation(const aiAnimation *anim) {
 		vector<KeyFrame> frames;
-		shared_ptr<map<string, JointTransform>> pose;
-		//add all poses to frames
+		shared_ptr<map<string, JointTransform>> pose = make_shared<map<string, JointTransform>>();
+		map<double, shared_ptr<map<string, JointTransform>>> timePosMap;
+		float time = 999999999;
+		//
 		for (int j=0; j<anim->mNumChannels; j++) {
 			aiNodeAnim *anode = anim->mChannels[j];
-			pose = make_shared<map<string, JointTransform>>(); //pose maps joint transforms to joints
-			float time = 999999999;
-			//add all JointTransforms to pose
+			//
 			for (int k=0; k<anode->mNumPositionKeys; k++) {
 				time = anode->mPositionKeys[k].mTime;
 				vec3 position = vec3_cast(anode->mPositionKeys[k].mValue);
 				quat rotation = quat_cast(anode->mRotationKeys[k].mValue);
-				(*pose)[anode->mNodeName.C_Str()] = JointTransform(position, rotation);
+				if (timePosMap.find(time) == timePosMap.end()) {
+					timePosMap[time] = pose;
+					pose = make_shared<map<string, JointTransform>>(); //pose maps joint transforms to joint names
+				}
+				else {
+					(*timePosMap[time])[anode->mNodeName.C_Str()] = JointTransform(position, rotation);
+				}
 			}
-			frames.push_back(KeyFrame(time, *pose));
 		}
+		for (map<double, shared_ptr<map<string, JointTransform>>>::iterator it = timePosMap.begin(); it != timePosMap.end(); ++it)
+			frames.push_back(KeyFrame(it->first, *(it->second)));
+		cout << timePosMap.size() << endl;
 		return Animation((float) (anim->mDuration*anim->mTicksPerSecond), frames);
 	}
 
 	void createAnimations(const aiScene *scene, vector<Animation> &animList) {
 		for (int i=0; i<scene->mNumAnimations; i++) {
 			animList.push_back(createAnimation(scene->mAnimations[i]));
+		}
+	}
+
+	void printAnimations(vector<Animation> &animList) {
+		for (int i=0; i<animList.size(); i++) {
+			cout << "animation " << i << " length: " << animList[i].length << endl;
+			for (int j=0; j<animList[i].frames.size(); j++) {
+				cout << "keyframe " << j << " has " << animList[i].frames[j].pose.size() << " joints" << " timeStamp: " << animList[i].frames[j].timeStamp << endl;
+				for (map<string, JointTransform>::iterator it = animList[i].frames[j].pose.begin(); it != animList[i].frames[j].pose.end(); ++it) {
+					cout << "joint " << it->first << endl;
+				}
+			}
 		}
 	}
 
@@ -404,12 +424,13 @@ public:
 		// cout << "jointMap size: " << jointMap->size() << endl;
 		buildJointHeirarchy(jointMap, scene->mRootNode, scene);
 		createAnimations(scene, animList);
+		// printAnimations(animList);
 		// printAllJoints(jointMap);
 
 
 		if (jointMap->size() > 0) {
 			rootJoint = getRootJoint(jointMap, scene->mRootNode)->children[0];
-			printJoints(rootJoint);
+			// printJoints(rootJoint);
 		}
 
 		cout << "num Meshes: " << scene->mNumMeshes << endl;
