3de48a7d71d50cca9a3798c31b50a2ea480b5326
bbdunning <40685582+bbdunning@users.noreply.github.com>
2020-06-01 23:28:05 -0700

    added UI

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,10 @@
 #include <irrklang/irrKlang.h>
 #include "ik_ISoundEngine.h"
 
+#include <imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/string_cast.hpp>
@@ -812,10 +816,6 @@ public:
 		drawSkybox(Model, Projection);
 
 		pokeballBody->forceActivationState(1);
-		if (inputHandler->Ctrlflag)
-			dx += .1;
-		if (inputHandler->Cflag)
-			dx -= .1;
 		if (inputHandler->Cflag) {
 			deletePhysicsObject(pokeballBody);
 			pokeballBody = createBall();
@@ -957,20 +957,90 @@ int main(int argc, char *argv[])
 	application->initPhysics();
 
 	// glfwSetWindowMonitor(windowManager->getHandle(), glfwGetPrimaryMonitor(), 0, 0, 2650, 1440, 144);
+	#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+		bool err = gl3wInit() != 0;
+	#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+		bool err = gladLoadGL() == 0;
+	#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
+		bool err = false;
+		glbinding::Binding::initialize();
+	#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
+		bool err = false;
+		glbinding::initialize([](const char* name) { return (glbinding::ProcAddress)glfwGetProcAddress(name); });
+	#endif
+	    if (err)
+    {
+        fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+        return 1;
+    }
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+
+    // Setup Platform/Renderer bindings
+	// const char* glsl_version = "#version 330 core";
+	const char* glsl_version = "#version 330";
+    ImGui_ImplGlfw_InitForOpenGL(windowManager->getHandle(), true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+	bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 	// Loop until the user closes the window.
 	while (! glfwWindowShouldClose(windowManager->getHandle()))
 	{
 		// Render scene.
-		glfwSetInputMode(windowManager->getHandle(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+		if (!application->inputHandler->Ctrlflag)
+			glfwSetInputMode(windowManager->getHandle(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+		else 
+			glfwSetInputMode(windowManager->getHandle(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+
 		application->camera.setViewAngles(windowManager->getHandle());
 		application->player1->setViewAngles(windowManager->getHandle());
 		application->render();
 
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+		
+		ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		// ImGui::EndFrame();
+
 		// Swap front and back buffers.
 		glfwSwapBuffers(windowManager->getHandle());
 		// Poll for and process events.
-		
 		glfwPollEvents();
 	}
 
